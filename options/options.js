d7598a00f63ecd4abca39f28969064978f811b08
Crowdbotics <team@crowdbotics.com>
2023-08-08 04:31:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-mouse-42718",
+  url: "https://weathered-mouse-42718.botics.co",
+  api: "https://weathered-mouse-42718.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
